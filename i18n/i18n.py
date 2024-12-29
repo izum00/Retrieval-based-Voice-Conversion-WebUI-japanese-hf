37a60d2bbb3cfc5f42c269b9a3a3e94e0a7710a9
izum00 <karisute14@gmail.com>
2024-12-30 08:10:07 +0900

    Update i18n.py

--- i18n/i18n.py
+++ i18n/i18n.py
@@ -1,5 +1,5 @@
-import json
 import locale
+import json
 import os
 
 
@@ -11,17 +11,12 @@ def load_language_list(language):
 
 class I18nAuto:
     def __init__(self, language=None):
-        if language in ["Auto", None]:
-            language = locale.getdefaultlocale()[
-                0
-            ]  # getlocale can't identify the system's language ((None, None))
-        if not os.path.exists(f"./i18n/locale/{language}.json"):
-            language = "en_US"
+        language = "ja_JP"
         self.language = language
         self.language_map = load_language_list(language)
 
     def __call__(self, key):
         return self.language_map.get(key, key)
 
-    def __repr__(self):
-        return "Use Language: " + self.language
+    def print(self):
+        print("Use Language:", self.language)
